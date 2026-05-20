856d29e8c1cad55c4dc4fe2baa877b7525e94316
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-05-20 11:52:02 +0600

    added the index js and create the api

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+
+const uri = process.env.MONGODB_URL;
+const app = express();
+const port = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("sportNest")
+    const facilityCollection = db.collection("facility")
+     
+    app.post('/add-facility', async(req,res) => {
+        const facilityData = req.body
+        const result = await facilityCollection.insertOne(facilityData)
+
+        res.json(result)
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
